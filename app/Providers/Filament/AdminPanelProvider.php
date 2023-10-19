3f028101ef77a00b3ac0686467197f51380036c1
Diego Orlonski <orlonski@icloud.com>
2023-10-18 23:02:54 -0300

    atu

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,12 +28,18 @@ class AdminPanelProvider extends PanelProvider
             ->login(Login::class)
             ->registration()
             ->passwordReset()
-            ->emailVerification()
+            // ->emailVerification()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
+            ->plugins(
+                [
+                    \Hasnayeen\Themes\ThemesPlugin::make(),
+                    \FilipFonal\FilamentLogManager\FilamentLogManager::make(),
+                ]
+            )
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
@@ -54,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                \Hasnayeen\Themes\Http\Middleware\SetTheme::class
             ])
             ->authMiddleware([
                 Authenticate::class,
